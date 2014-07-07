37bfbb1a6d6554f6fcf876572c8fd2e47673693a
gdharley <greg.harley@gmail.com>
2014-07-08 07:41:48 +1000

    Added Spiral Test

--- app/controllers/snapshot_controller.rb
+++ app/controllers/snapshot_controller.rb
@@ -3,8 +3,12 @@ class SnapshotController < ApplicationController
     @processApp = ProcessApp.find(params[:pid])
     @snapshot = Snapshot.find(params[:sid])
 
-    @foo = []
-    @snapshot.findDependencies(params[:sid], @foo)
+    @deps = []   # list of dependant snapshots
+    @cyclic = [] # List of cyclic dependencies (potential issue)
+    @spiral =[]  # List of illegal "spiral" dependencies
+    @depTk = {}  # Map (processApp.id : snapshot.id})of dependant toolkits. Used to determine spiral dependencies
+
+    @snapshot.findDependencies(params[:sid], @deps, @cyclic, @spiral, @depTk)
 
 
     render "/snapshot/show"
--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -7,14 +7,29 @@ class Snapshot < ActiveRecord::Base
 
   attr_accessible :guid, :name, :process_app_id, :type
 
-  def findDependencies(sid, dList)
+  def findDependencies(sid, dList, cyclic, spiral, depTks)
 
     @dependencies = Dependency.find_all_by_proc_app_snap_id(sid)
     @dependencies.each do |dependency|
+      # retrieve dependant snapshot from the "child" id
       snapshot = Snapshot.find(dependency.toolkit_snap_id)
+
+      if depTks.has_key?(Toolkit.find(snapshot.process_app_id).id) && depTks[Toolkit.find(snapshot.process_app_id).id] != snapshot.id
+        spiral << snapshot
+      else
+        depTks[Toolkit.find(snapshot.process_app_id).id] = snapshot.id
+      end
+
+      # add to list of dependencies
       if !dList.include?(snapshot)
+        # check if process app/toolkit reference exists
         dList << snapshot
-        self.findDependencies(snapshot.id, dList)
+        self.findDependencies(snapshot.id, dList, cyclic, spiral, depTks)
+      else
+        # Snapshot already listed as a dependency, add it to the cyclic array
+        if !cyclic.include?(snapshot)
+          cyclic << snapshot
+        end
       end
     end
 
